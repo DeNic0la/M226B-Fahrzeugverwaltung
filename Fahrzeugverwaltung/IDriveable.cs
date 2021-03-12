83e8020f42c19904c907d93b1b84ed093c71a78a
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2021-03-12 09:20:46 +0100

    Added all the Class files and the Interface.
    No Actual code written

---
+++ Fahrzeugverwaltung/IDriveable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrzeugverwaltung {
+    interface IDriveable {
+        void setOwner(Human newOwner);
+        Human getOwner();
+    }
+}
---
+++ Fahrzeugverwaltung/Motorfahzeug.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrzeugverwaltung {
+    abstract class Motorfahzeug : IDriveable {
+
+        public Motor Motor;
+        public Human getOwner() {
+            throw new NotImplementedException();
+        }
+
+        public void setOwner(Human newOwner) {
+            throw new NotImplementedException();
+        }
+    }
+}
