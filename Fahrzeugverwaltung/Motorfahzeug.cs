f7531679b17e1f407d1078ba3bf68f7ccd2ad98b
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2021-03-12 11:20:01 +0100

    Update Motorfahzeug.cs

--- Fahrzeugverwaltung/Motorfahzeug.cs
+++ Fahrzeugverwaltung/Motorfahzeug.cs
@@ -8,12 +8,25 @@ namespace Fahrzeugverwaltung {
     abstract class Motorfahzeug : IDriveable {
 
         public Motor Motor;
+        public string Kontrollschild;
+        public string Marke;
+        public string Typ;
+
+
+
+
         public Human getOwner() {
             throw new NotImplementedException();
         }
 
         public void setOwner(Human newOwner) {
             throw new NotImplementedException();
+        }
+
+        public Motorfahzeug(string seKontrollschild, string seMarke, string seTyp) {
+            Kontrollschild = seKontrollschild;
+            Marke = seMarke;
+            Typ = seTyp;
         }
     }
 }
